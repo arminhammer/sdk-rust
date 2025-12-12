af955935be065ec6cb7012e11abaf2f2f9970cc8
Armin Graf <arminhammer@gmail.com>
2025-12-11 22:11:35 -0500

    wip

--- core/src/models/extension.rs
+++ core/src/models/extension.rs
@@ -1,11 +1,23 @@
-use serde_derive::{Deserialize, Serialize};
-use std::collections::HashMap;
+use crate::models::map::*;
 use crate::models::task::*;
+use serde_derive::{Deserialize, Serialize};
 
 /// Represents the definition of a an extension
-#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
-pub struct ExtensionDefinition{
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum ExtensionDefinition {
+    Named(std::collections::HashMap<String, ExtensionFields>),
+}
 
+impl Default for ExtensionDefinition {
+    fn default() -> Self {
+        ExtensionDefinition::Named(std::collections::HashMap::new())
+    }
+}
+
+/// Represents the fields of an extension definition
+#[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ExtensionFields {
     /// Gets/sets the type of task to extend
     #[serde(rename = "extend")]
     pub extend: String,
@@ -14,12 +26,11 @@ pub struct ExtensionDefinition{
     #[serde(rename = "when", skip_serializing_if = "Option::is_none")]
     pub when: Option<String>,
 
-    /// Gets/sets a name/definition map, if any, of the tasks to execute before the extended task
+    /// Gets/sets a name/definition mapping, if any, of the tasks to execute before the extended task
     #[serde(rename = "before", skip_serializing_if = "Option::is_none")]
-    pub before: Option<HashMap<String, TaskDefinition>>,
+    pub before: Option<Map<String, TaskDefinition>>,
 
-    /// Gets/sets a name/definition map, if any, of the tasks to execute after the extended task
+    /// Gets/sets a name/definition mapping, if any, of the tasks to execute after the extended task
     #[serde(rename = "after", skip_serializing_if = "Option::is_none")]
-    pub after: Option<HashMap<String, TaskDefinition>>
-
+    pub after: Option<Map<String, TaskDefinition>>,
 }
--- core/src/models/workflow.rs
+++ core/src/models/workflow.rs
@@ -1,6 +1,3 @@
-use serde_derive::{Deserialize, Serialize};
-use serde_json::Value;
-use std::collections::HashMap;
 use crate::models::authentication::*;
 use crate::models::catalog::*;
 use crate::models::duration::*;
@@ -13,6 +10,9 @@ use crate::models::output::*;
 use crate::models::retry::*;
 use crate::models::task::*;
 use crate::models::timeout::*;
+use serde_derive::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
 
 /// Gets the namespace to use by default for workflow definitions
 pub const DEFAULT_NAMESPACE: &str = "default";
@@ -29,7 +29,7 @@ fn default_dsl_version() -> String {
 }
 
 // Provides the default runtime expression language
-fn default_runtime_expression_language() -> String{
+fn default_runtime_expression_language() -> String {
     RuntimeExpressionLanguage::JQ.to_string()
 }
 
@@ -44,8 +44,7 @@ impl RuntimeExpressionLanguage {
 
 /// Represents the definition of a workflow
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
-pub struct WorkflowDefinition{
-
+pub struct WorkflowDefinition {
     /// Gets/sets an object used to document the defined workflow
     #[serde(rename = "document")]
     pub document: WorkflowDefinitionMetadata,
@@ -80,32 +79,28 @@ pub struct WorkflowDefinition{
 
     /// Gets/sets a key/value mapping, if any, of additional information associated with the workflow
     #[serde(rename = "metadata", skip_serializing_if = "Option::is_none")]
-    pub metadata: Option<HashMap<String, Value>>
-
+    pub metadata: Option<HashMap<String, Value>>,
 }
 impl WorkflowDefinition {
-
     // Initializes a new workflow definition
     pub fn new(document: WorkflowDefinitionMetadata) -> Self {
-        Self { 
-            document, 
+        Self {
+            document,
             input: None,
             use_: None,
-            timeout: None, 
+            timeout: None,
             output: None,
             schedule: None,
             evaluate: None,
             do_: Map::new(),
-            metadata: None
+            metadata: None,
         }
     }
-    
 }
 
 /// Represents the metadata of a workflow, including its name, version, and description.
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
 pub struct WorkflowDefinitionMetadata {
-    
     /// Gets/sets the version of the DSL used to define the workflow
     #[serde(rename = "dsl")]
     pub dsl: String,
@@ -134,29 +129,33 @@ pub struct WorkflowDefinitionMetadata {
 
     /// Gets/sets a key/value mapping of the workflow's tags, if any
     #[serde(rename = "tags", skip_serializing_if = "Option::is_none")]
-    pub tags: Option<HashMap<String, String>>
+    pub tags: Option<HashMap<String, String>>,
 }
-impl WorkflowDefinitionMetadata{
-
+impl WorkflowDefinitionMetadata {
     // Initializes a new workflow definition metadata
-    pub fn new(namespace : &str, name: &str, version : &str, title: Option<String>, summary : Option<String>, tags: Option<HashMap<String, String>>) -> Self {
-        Self { 
+    pub fn new(
+        namespace: &str,
+        name: &str,
+        version: &str,
+        title: Option<String>,
+        summary: Option<String>,
+        tags: Option<HashMap<String, String>>,
+    ) -> Self {
+        Self {
             dsl: default_dsl_version(),
             namespace: namespace.to_owned(),
             name: name.to_owned(),
             version: version.to_owned(),
             title,
             summary,
-            tags
+            tags,
         }
     }
-
 }
 
 /// Represents the definition of a workflow's schedule
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
-pub struct WorkflowScheduleDefinition{
-
+pub struct WorkflowScheduleDefinition {
     /// Gets/sets an object used to document the defined workflow
     #[serde(rename = "every", skip_serializing_if = "Option::is_none")]
     pub every: Option<Duration>,
@@ -171,28 +170,24 @@ pub struct WorkflowScheduleDefinition{
 
     /// Gets/sets the events that trigger the workflow execution
     #[serde(rename = "on", skip_serializing_if = "Option::is_none")]
-    pub on: Option<EventConsumptionStrategyDefinition>
-
+    pub on: Option<EventConsumptionStrategyDefinition>,
 }
 
 /// Represents an object used to configure the workflow's runtime expression evaluation
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
-pub struct RuntimeExpressionEvaluationConfiguration{
-
+pub struct RuntimeExpressionEvaluationConfiguration {
     /// Gets/sets the language used for writing runtime expressions
     #[serde(rename = "language", default = "default_runtime_expression_language")]
     pub language: String,
 
     /// Gets/sets the language used for writing runtime expressions
     #[serde(rename = "mode", skip_serializing_if = "Option::is_none")]
-    pub mode: Option<String>
-
+    pub mode: Option<String>,
 }
 
 /// Represents a collection of workflow components
 #[derive(Debug, Default, Clone, PartialEq, Serialize, Deserialize)]
-pub struct ComponentDefinitionCollection{
-
+pub struct ComponentDefinitionCollection {
     /// Gets/sets a name/value mapping of the workflow's reusable authentication policies
     #[serde(rename = "authentications", skip_serializing_if = "Option::is_none")]
     pub authentications: Option<HashMap<String, AuthenticationPolicyDefinition>>,
@@ -207,7 +202,7 @@ pub struct ComponentDefinitionCollection{
 
     /// Gets/sets a name/value mapping of the workflow's extensions, if any
     #[serde(rename = "extensions", skip_serializing_if = "Option::is_none")]
-    pub extensions: Option<HashMap<String, ExtensionDefinition>>,
+    pub extensions: Option<Vec<ExtensionDefinition>>,
 
     /// Gets/sets a name/value mapping of the workflow's reusable functions
     #[serde(rename = "functions", skip_serializing_if = "Option::is_none")]
@@ -224,5 +219,4 @@ pub struct ComponentDefinitionCollection{
     /// Gets/sets a name/value mapping of the workflow's reusable timeouts
     #[serde(rename = "timeouts", skip_serializing_if = "Option::is_none")]
     pub timeouts: Option<HashMap<String, TimeoutDefinition>>,
-
 }
